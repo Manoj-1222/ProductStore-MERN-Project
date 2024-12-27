a855c78d7c2f0d19a2a5a02b4bcbb1a382506205
Gogurla Manoj <gogurlamanoj722@gmail.com>
2024-12-27 09:36:39 +0530

    changes made in server,js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,26 @@
-const express = require('express')
-// import express from "express";
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.route.js"
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-console.log(__dirname);
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
-// console.log(process.env.MONGO_URI)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname + "/frontend" + "/dist" + "/index.html"))
-    })
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
-    connectDB()
-    console.log("Server started at http://localhost:" + PORT);
-})
-// 
+    connectDB();
+    console.log(`Server started at http://localhost:${PORT}`);
+});
